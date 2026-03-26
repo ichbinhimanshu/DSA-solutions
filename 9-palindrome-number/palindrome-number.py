10196675db0cdd23cb10614d871c074f059e4e59
Himanshu Singh <himanshusingh700122@gmail.com>
2026-03-26 10:40:10 +0530

    Implement palindrome number checker in Python

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0 or (x%10==0 and x!=0):
+            return False
+        else:
+            rev=0
+            while(x>rev):
+                d=x%10
+                rev=rev*10+d
+                x=x//10
+            if rev==x or x==rev//10:  #121 x=1 and rev=12 so use rev//10
+                return True
+            else:
+                return False
