58201da8cf4cbc68a6ddf947330ee99d5aef249e
Himanshu Singh <himanshusingh700122@gmail.com>
2026-03-28 12:32:19 +0530

    Time: 4 ms (41.79%) | Memory: 13.2 MB (96.43%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,14 +4,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # i=0
+        # while(i<len(nums)):
+        #     j=i+1
+        #     while(j<len(nums)):
+        #         if nums[i]==nums[j]:
+        #             nums.pop(j)
+        #         else:
+        #             j+=1
+        #     i+=1
+        # return len(nums)
+
+
+        # x=list(set(nums))
+        # y=sorted(x)
+        # for i in range(len(x)):
+        #     nums[i]=x[i]
+        # return len(x)
+
         i=0
-        while(i<len(nums)):
-            j=i+1
-            while(j<len(nums)):
-                if nums[i]==nums[j]:
-                    nums.pop(j)
-                else:
-                    j+=1
-            i+=1
-        return len(nums)
+        j=1
+        while(j<len(nums)):
+            if(nums[i]!=nums[j]):
+                i+=1
+                nums[i]=nums[j]
+            j+=1
+        return i+1
+
+
                 
