7bcd915557976a23324367f4c3a404d267261374
Himanshu Singh <himanshusingh700122@gmail.com>
2026-03-29 13:56:11 +0530

    Time: 7 ms (16.03%) | Memory: 12.6 MB (53.71%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,17 +1,25 @@
 class Solution(object):
     def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         d={
-            ")":"(",
-            "]":"[",
-            "}":"{",
+            "(":")",
+            "[":"]",
+            "{":"}",
         }
+        
+        if s[0] in d.values():
+            return False
+        
         i=0
         result=[]
         while(i<len(s)):
-            if s[i] in d.values():
+            if s[i] in d.keys():
                 result.append(s[i])
             else:
-                if result and result[-1]==d[s[i]]:
+                if len(result)!=0 and s[i]==d[result[-1]]:
                     result.pop()
                 else:
                     return False
