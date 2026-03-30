93665c66e6e4184aceadd263da30f14fb1af8338
Himanshu Singh <himanshusingh700122@gmail.com>
2026-03-30 12:23:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (94.71%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -7,11 +7,9 @@ class Solution(object):
         """
         i=0
         while(i<len(nums)):
-            if(nums[i]==val):
+            if (nums[i]==val):
                 nums[i],nums[-1]=nums[-1],nums[i]
                 nums.pop()
             else:
                 i+=1
         return len(nums)
-
-        
