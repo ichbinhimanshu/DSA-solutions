78cc2be982303c9ab97e2e16b574675c72c92759
Himanshu Singh <himanshusingh700122@gmail.com>
2026-04-24 19:48:56 +0530

    Time: 12 ms (75.90%) | Memory: 12.4 MB (55.59%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.py
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -1,13 +1,22 @@
+# class Solution(object):
+#     def defangIPaddr(self, address):
+#         """
+#         :type address: str
+#         :rtype: str
+#         """
+#         dum=""
+#         for i in address:
+#             if i==".":
+#                 dum=dum+"[.]"
+#             else:
+#                 dum=dum+i
+#         return dum
 class Solution(object):
     def defangIPaddr(self, address):
-        """
-        :type address: str
-        :rtype: str
-        """
-        dum=""
-        for i in address:
-            if i==".":
-                dum=dum+"[.]"
+        res = []
+        for ch in address:
+            if ch == ".":
+                res.append("[.]")
             else:
-                dum=dum+i
-        return dum
+                res.append(ch)
+        return "".join(res)
