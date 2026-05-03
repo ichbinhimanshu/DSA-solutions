09aeeaffe29d9c306e9217b91b40f807dc3e654c
Himanshu Singh <himanshusingh700122@gmail.com>
2026-05-03 11:39:47 +0530

    Time: 15 ms (76.36%) | Memory: 25.8 MB (77.80%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s=set(nums)
+        return len(nums)>len(s)    
