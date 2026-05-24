acda7476b6cc7824f83e6623ad6b979981bbc91b
Himanshu Singh <himanshusingh700122@gmail.com>
2026-05-24 11:49:46 +0530

    Time: 3 ms (27.03%) | Memory: 12.4 MB (22.38%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        fa=[]
+        fb=[]
+        fc=[]
+        for i in range(1,a+1):
+            if a%i==0:
+                fa.append(i)
+        for i in range(1,b+1):
+            if b%i==0:
+                fb.append(i)
+        for i in fa:
+            if i in fb:
+                fc.append(i)
+        return len(fc)
