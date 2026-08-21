cf55fa5107d6bc9bd88be2b690b65730be45e421
Himanshu Singh <himanshusingh700122@gmail.com>
2026-08-21 09:16:54 +0530

    Time: 24 ms (28.49%) | Memory: 81.4 MB (91.96%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,16 +1,35 @@
 import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer,Integer> count = new HashMap<>();
-
-        for(int i=0;i<nums.length;i++){
-            if(count.containsKey(nums[i])){
-                return true;
+        Arrays.sort(nums);
+        int i = 0;
+        int j = 1;
+        while(j<nums.length){
+            if(nums[i]!=nums[j]){
+                i++;
+                j++;
             }
             else{
-                count.put(nums[i],i);
+                return true;
             }
         }
         return false;
     }
 }
+
+// import java.util.*;
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashMap<Integer,Integer> count = new HashMap<>();
+
+//         for(int i=0;i<nums.length;i++){
+//             if(count.containsKey(nums[i])){
+//                 return true;
+//             }
+//             else{
+//                 count.put(nums[i],i);
+//             }
+//         }
+//         return false;
+//     }
+// }
