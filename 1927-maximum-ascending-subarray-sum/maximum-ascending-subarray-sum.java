09fd6dc7740b32453a19a1e92be97c31c3937b88
Himanshu Singh <himanshusingh700122@gmail.com>
2026-08-24 22:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (81.30%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int check1 = nums[0];
+        int check2 = nums[0];
+        int i=0;
+
+        while(i<nums.length-1){
+            if(nums[i]<nums[i+1]){
+                i++;
+                check1 = check1 + nums[i];
+            }
+            else{
+                i++;
+                check1 = nums[i];
+            }
+
+            if(check1 > check2){
+                check2 = check1;
+            }
+        }
+        return check2;
+    }
+}
