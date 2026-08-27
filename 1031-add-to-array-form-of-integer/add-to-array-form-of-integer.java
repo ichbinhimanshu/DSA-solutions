68e4089eb741205760b3ede35f5f63a4a7f6481e
Himanshu Singh <himanshusingh700122@gmail.com>
2026-08-27 10:39:48 +0530

    Time: 3 ms (73.83%) | Memory: 47.5 MB (84.15%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -1,25 +1,71 @@
-import java.math.BigInteger;
-import java.util.*;
+// class Solution {
+//     public List<Integer> addToArrayForm(int[] num, int k) {
+//         List<Integer> ans = new ArrayList<>();
+//         int i = num.length - 1;
+//         int carry = 0;
+
+//         while(i>=0 || k>0){
+//             int sum = carry;
+//             if(i >= 0){
+//                 sum+=num[i--];
+//             }
+//             if(k>0){
+//                 sum+=k%10;
+//                 k/=10;
+//             }
+//             ans.add(sum%10);
+//             carry = sum/10;
+//         }
+//         if(carry>0){
+//             ans.add(carry);
+//         }
+
+//         Collections.reverse(ans);
+//         return ans;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        String emp = "";
+        List<Integer> ans = new ArrayList<>();
 
-        for (int i = 0; i < num.length; i++) {
-            emp = emp + Integer.toString(num[i]);
+        int p = num.length-1;
+        int carry = 0;
+
+        while(p>=0 || k>0){
+            int sum = carry;       // if i = -1 so it give error so we need some value atleast
+
+            if(p>=0){             //only i-- when need
+                sum = sum + num[p];
+                p--;
+            }
+            if(k>0){
+                sum = sum + (k%10);
+                k=k/10;
+            }
+
+            ans.add(sum%10);
+            carry = sum/10;
         }
 
-        BigInteger n = new BigInteger(emp);
-        n = n.add(BigInteger.valueOf(k));
-
-        String result = n.toString();
-
-        List<Integer> x = new ArrayList<>();
-
-        for (int i = 0; i < result.length(); i++) {
-            x.add(Integer.parseInt(String.valueOf(result.charAt(i))));
+        if(carry>0){                        // first digit if we have carry left add it
+            ans.add(carry);
         }
 
-        return x;
+        Collections.reverse(ans);
+        return ans;
     }
 }
