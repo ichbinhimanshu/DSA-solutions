6b954c15369fcfea7fd7998086f1605cad2b60ca
Himanshu Singh <himanshusingh700122@gmail.com>
2026-09-11 14:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (83.41%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,26 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         String word[] = s.split(" ");
+//         return word[word.length-1].length();
+//     }
+// }
 class Solution {
     public int lengthOfLastWord(String s) {
-        String word[] = s.split(" ");
-        return word[word.length-1].length();
+        int len = 0;
+        int i=s.length()-1;
+        while(i>=0){
+            if(s.charAt(i)==' '){
+                i--;
+            }
+            else{
+                break;
+            }
+        }
+
+        while(i>=0 && s.charAt(i)!=' '){
+            len++;
+            i--;
+        }
+        return len;
     }
 }
