a47c179df81497efe038912e4dee7cc3a0aaa21b
Himanshu Singh <himanshusingh700122@gmail.com>
2026-09-12 13:38:45 +0530

    Time: 1 ms (99.65%) | Memory: 43 MB (32.62%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,47 +1,76 @@
+// class Solution {
+//     public int lastStoneWeight(int[] stones) {
+//         List<Integer> list = new ArrayList<>();
+        
+//         for(int i:stones){
+//             list.add(i);
+//         }
+
+//         if(list.size()==1){
+//             return list.get(0);
+//         }
+
+//         if(list.size()==2){
+//             return Math.abs(list.get(0) - list.get(1));
+//         }
+
+//         while(list.size()>1){
+//             int max = -1;
+//             int smax = -1;
+
+//             for(int i:list){
+//                 if(i>max){
+//                     smax=max;
+//                     max=i;
+//                 }
+//                 else if(i>smax){
+//                     smax=i;
+//                 }
+//             }
+
+//             int diff = max-smax;
+//             list.remove(Integer.valueOf(max));
+//             list.remove(Integer.valueOf(smax));
+
+
+//             if(diff>0){
+//                 list.add(diff);
+//             }
+//         }
+//         if(list.isEmpty()){
+//             return 0;
+//         }
+//         else{
+//             return list.get(0);
+//         }
+//     }
+// }
+
+
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        List<Integer> list = new ArrayList<>();
-        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
         for(int i:stones){
-            list.add(i);
+            pq.add(i);
         }
 
-        if(list.size()==1){
-            return list.get(0);
-        }
+        while(pq.size()>1){
+            int max = pq.poll();
+            int smax = pq.poll();
 
-        if(list.size()==2){
-            return Math.abs(list.get(0) - list.get(1));
-        }
-
-        while(list.size()>1){
-            int max = -1;
-            int smax = -1;
-
-            for(int i:list){
-                if(i>max){
-                    smax=max;
-                    max=i;
-                }
-                else if(i>smax){
-                    smax=i;
-                }
-            }
-
-            int diff = max-smax;
-            list.remove(Integer.valueOf(max));
-            list.remove(Integer.valueOf(smax));
-
-
+            int diff=max-smax;
+            
             if(diff>0){
-                list.add(diff);
+                pq.add(diff);
             }
+
         }
-        if(list.isEmpty()){
+        if(pq.size()==0){
             return 0;
         }
         else{
-            return list.get(0);
+            return pq.peek();
         }
     }
 }
