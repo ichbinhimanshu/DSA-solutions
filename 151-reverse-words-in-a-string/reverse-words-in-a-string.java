918c697a40d88f49513e8222faf379adfcd14f5b
Himanshu Singh <himanshusingh700122@gmail.com>
2026-09-13 11:37:06 +0530

    Time: 11 ms (10.83%) | Memory: 46.9 MB (10.46%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,51 +1,19 @@
-// class Solution {
-//     public String reverseWords(String s) {
-//         String arr[] = s.split("\\s+");   //  \> skip , s> space, +> multiple
-//         String ans = "";
+class Solution {
+    public String reverseWords(String s) {
+        String arr[] = s.split("\\s+");   //  \> skip , s> space, +> multiple
+        String ans = "";
 
-//         for(int i=arr.length-1;i>=0;i--){
-//             if(ans==""){
-//                 ans = ans+ arr[i];
-//             }
-//             else{
-//                 ans = ans + " " + arr[i];
-//             }
-//         }
-//         return ans.trim();
-//     }
-// }
+        for(int i=arr.length-1;i>=0;i--){
+            if(ans==""){
+                ans = ans+ arr[i];
+            }
+            else{
+                ans = ans + " " + arr[i];
+            }
+        }
+        return ans.trim();
+    }
+}
 
 // //trim --> remove starting and last spaces
 
-
-class Solution {
-    public String reverseWords(String s) {
-        s = s.trim();
-
-        StringBuilder ans = new StringBuilder();
-        int j = s.length() - 1;
-
-        while (j >= 0) {
-            int i = j;
-
-            while (i >= 0 && s.charAt(i) != ' ') {
-                i--;
-            }
-
-            if (ans.length() > 0) {
-                ans.append(' ');
-            }
-
-            ans.append(s, i + 1, j + 1);
-
-            // Skip spaces between words
-            while (i >= 0 && s.charAt(i) == ' ') {
-                i--;
-            }
-
-            j = i;
-        }
-
-        return ans.toString();
-    }
-}
