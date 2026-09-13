84a9fcce8810751b735667a47a72ca9b0f44f382
Himanshu Singh <himanshusingh700122@gmail.com>
2026-09-13 11:35:28 +0530

    Time: 11 ms (10.83%) | Memory: 46.8 MB (16.03%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,34 +1,19 @@
-// class Solution {
-//     public String reverseWords(String s) {
-//         String arr[] = s.split("\\s+");   //  \> skip , s> space, +> multiple
-//         String ans = "";
+class Solution {
+    public String reverseWords(String s) {
+        String arr[] = s.split("\\s+");   //  \> skip , s> space, +> multiple
+        String ans = "";
 
-//         for(int i=arr.length-1;i>=0;i--){
-//             if(ans==""){
-//                 ans = ans+ arr[i];
-//             }
-//             else{
-//                 ans = ans + " " + arr[i];
-//             }
-//         }
-//         return ans.trim();
-//     }
-// }
+        for(int i=arr.length-1;i>=0;i--){
+            if(ans==""){
+                ans = ans+ arr[i];
+            }
+            else{
+                ans = ans + " " + arr[i];
+            }
+        }
+        return ans.trim();
+    }
+}
 
 // //trim --> remove starting and last spaces
 
-class Solution {
-    public String reverseWords(String s) {
-        String[] arr = s.trim().split("\\s+");
-        StringBuilder ans = new StringBuilder();
-
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (ans.length() > 0) {
-                ans.append(" ");
-            }
-            ans.append(arr[i]);
-        }
-
-        return ans.toString();
-    }
-}
