1a81f461012c90ca9a1894ff87e928b78390d8a8
Himanshu Singh <himanshusingh700122@gmail.com>
2026-09-19 23:51:11 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (67.86%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -1,19 +1,35 @@
+// class Solution {
+//     public int[][] transpose(int[][] matrix) {
+//         //square matrix
+//         if(matrix.length == matrix[0].length){
+//             int[][] ans = new int[matrix.length][matrix.length];
+
+//             for(int i=0;i<matrix.length;i++){
+//                 for(int j=0;j<matrix.length;j++){
+//                     ans[j][i] = matrix[i][j];
+//                 }
+//             }
+
+//             return ans;
+//         }
+//         //rectangular matrix
+//         else{
+//             int[][] ans = new int[matrix[0].length][matrix.length];
+
+//             for(int i=0;i<matrix.length;i++){
+//                 for(int j=0;j<matrix[0].length;j++){
+//                     ans[j][i] = matrix[i][j];
+//                 }
+//             }
+
+//             return ans;
+//         }
+//     }
+// }
+
+
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        //square matrix
-        if(matrix.length == matrix[0].length){
-            int[][] ans = new int[matrix.length][matrix.length];
-
-            for(int i=0;i<matrix.length;i++){
-                for(int j=0;j<matrix.length;j++){
-                    ans[j][i] = matrix[i][j];
-                }
-            }
-
-            return ans;
-        }
-        //rectangular matrix
-        else{
             int[][] ans = new int[matrix[0].length][matrix.length];
 
             for(int i=0;i<matrix.length;i++){
@@ -25,4 +41,3 @@ class Solution {
             return ans;
         }
     }
-}
