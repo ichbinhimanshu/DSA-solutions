df915da1a52a8572c8d279915939317a8b92b2eb
Himanshu Singh <himanshusingh700122@gmail.com>
2026-09-24 21:52:28 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (5.73%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -8,26 +8,53 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//         if(head == null || head.next == null){
+//             return head;
+//         }
+
+//         ListNode p1 = head;
+//         ListNode p2 = head;
+
+//         while(p2!=null){
+//             if(p1.val == p2.val){
+//                 p2=p2.next;
+//             }
+//             else{
+//                 p1.next = p2;
+//                 p1 = p2;
+//                 p2=p2.next;
+//             }
+//         }
+//         p1.next = null;
+//         return head;
+//     }
+// }
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if(head == null || head.next == null){
             return head;
         }
 
-        ListNode p1 = head;
-        ListNode p2 = head;
+        ListNode dummy = new ListNode(head.val);
+        ListNode ans = dummy;
 
-        while(p2!=null){
-            if(p1.val == p2.val){
-                p2=p2.next;
+        ListNode ptr = head.next;
+        while(ptr!=null){
+            if(ptr.val != dummy.val){
+                ListNode temp = new ListNode(ptr.val);
+                dummy.next = temp;
+                dummy = dummy.next;
+
+                ptr = ptr.next;
             }
             else{
-                p1.next = p2;
-                p1 = p2;
-                p2=p2.next;
+                ptr = ptr.next;
             }
+
         }
-        p1.next = null;
-        return head;
+        return ans;
     }
 }
